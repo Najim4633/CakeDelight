5eb4981b2748151513864117516b331f541fbbcc
Najim4633 <najimtamboli670@gmail.com>
2026-08-13 00:30:26 +0530

    feat: update service configurations for Docker and improve database connection settings

---
+++ docker/mysql/init.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS cakedelight_catalog;
+CREATE DATABASE IF NOT EXISTS cakedelight_order;
+CREATE DATABASE IF NOT EXISTS cakedelight_rating;
+CREATE DATABASE IF NOT EXISTS cakedelight_notification;
